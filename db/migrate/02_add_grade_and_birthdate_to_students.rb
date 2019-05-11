858ae8ceb1f9ae13ab8d8b1cc1e337adf9fb9c27
Ming Duan <11203duan@gmail.com>
2019-05-11 22:39:06 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,8 @@
 def CreateStudents < ActiveRecord::Migration[5.1]
 
 def change
-  add_column :students, :grade, INTEGER
+  add_column :students, :grade, :integer
+  add_column :students, :birthdate, :string
 end
 
 end
